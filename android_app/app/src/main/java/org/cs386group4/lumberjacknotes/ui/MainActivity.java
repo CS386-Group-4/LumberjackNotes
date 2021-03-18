193e8285909158e30b4a34ae4d2abf750c40b7cb
Nick Botticelli <nick.s.botticelli@gmail.com>
2021-03-17 21:23:31 -0700

    Continue setting up basic UI for application

--- android_app/app/src/main/java/org/cs386group4/lumberjacknotes/ui/MainActivity.java
+++ android_app/app/src/main/java/org/cs386group4/lumberjacknotes/ui/MainActivity.java
@@ -1,8 +1,14 @@
 package org.cs386group4.lumberjacknotes.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import org.cs386group4.lumberjacknotes.R;
 
@@ -13,5 +19,26 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the toolbar from layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        // initialize toolbar
+        setSupportActionBar(toolbar);
+        this.invalidateOptionsMenu();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.main_note_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
