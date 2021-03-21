2ed098bb389dbe166861d74ae3ea29c977ebaef0
Nick Botticelli <nick.s.botticelli@gmail.com>
2021-03-20 21:45:39 -0700

    Add new login page with new strings and updated controller

--- android_app/app/src/main/java/org/cs386group4/lumberjacknotes/controllers/LoginController.java
+++ android_app/app/src/main/java/org/cs386group4/lumberjacknotes/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package org.cs386group4.lumberjacknotes.controllers;
 
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -12,12 +13,30 @@ public class LoginController
 {
     public LoginController(LoginActivity loginActivity)
     {
-        MaterialButton loginButton = loginActivity.findViewById(R.id.login_login_button);
+        initLoginButton(loginActivity);
+        initRegisterButton(loginActivity);
+    }
+
+    private void initLoginButton(LoginActivity loginActivity)
+    {
+        MaterialButton loginButton = loginActivity.findViewById(R.id.login_button);
 
         loginButton.setOnClickListener(view ->
         {
+            // TODO: Handle login
             Intent intent = new Intent(loginActivity, MainActivity.class);
             loginActivity.startActivity(intent);
+        });
+    }
+
+    private void initRegisterButton(LoginActivity loginActivity)
+    {
+        MaterialButton registerButton = loginActivity.findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(view ->
+        {
+            // TODO: Handle registration
+            Toast.makeText(loginActivity, "Register button pressed", Toast.LENGTH_SHORT).show();
         });
     }
 }
