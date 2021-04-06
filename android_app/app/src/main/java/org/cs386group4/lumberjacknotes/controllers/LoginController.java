71ba5a9e5aebe4ed8482fb7d09646beb3e65e0cc
Nick Botticelli <nick.s.botticelli@gmail.com>
2021-04-05 19:11:14 -0700

    Add Javadoc-style comments to LoginController, Share, Tutorial, and UserPreferences

--- android_app/app/src/main/java/org/cs386group4/lumberjacknotes/controllers/LoginController.java
+++ android_app/app/src/main/java/org/cs386group4/lumberjacknotes/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package org.cs386group4.lumberjacknotes.controllers;
 
+import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
@@ -7,37 +8,54 @@ import com.google.android.material.button.MaterialButton;
 
 import org.cs386group4.lumberjacknotes.R;
 import org.cs386group4.lumberjacknotes.ui.LoginActivity;
-import org.cs386group4.lumberjacknotes.ui.MainActivity;
 import org.cs386group4.lumberjacknotes.ui.NotesListActivity;
 
+/**
+ * Controller to handle logging in with the {@link LoginActivity}
+ */
 public class LoginController
 {
+    /**
+     * Initialize the login controller
+     * @param loginActivity valid instance to get a {@link Context} from
+     */
     public LoginController(LoginActivity loginActivity)
     {
+        // Initialize login and register buttons
         initLoginButton(loginActivity);
         initRegisterButton(loginActivity);
     }
 
+    /**
+     * Initialize the login button such as by adding a click listener to it
+     * @param loginActivity valid {@link LoginActivity} instance to get a {@link Context} from
+     */
     private void initLoginButton(LoginActivity loginActivity)
     {
         MaterialButton loginButton = loginActivity.findViewById(R.id.login_button);
 
+        // Handle logging in upon user clicking the login button
         loginButton.setOnClickListener(view ->
         {
-            // TODO: Handle login
-            Intent intent = new Intent(loginActivity, MainActivity.class);
-            //Intent intent = new Intent(loginActivity, NotesListActivity.class);
+            // TODO: Handle real login from cloud database
+            //Intent intent = new Intent(loginActivity, MainActivity.class);
+            Intent intent = new Intent(loginActivity, NotesListActivity.class);
             loginActivity.startActivity(intent);
         });
     }
 
+    /**
+     * Initialize the register button such as by adding a click listener to it
+     * @param loginActivity valid instance to get a {@link Context} from
+     */
     private void initRegisterButton(LoginActivity loginActivity)
     {
         MaterialButton registerButton = loginActivity.findViewById(R.id.register_button);
 
+        // Handle registration upon user clicking the register button
         registerButton.setOnClickListener(view ->
         {
-            // TODO: Handle registration
+            // TODO: Handle registration with cloud
             Toast.makeText(loginActivity, "Register button pressed", Toast.LENGTH_SHORT).show();
         });
     }
