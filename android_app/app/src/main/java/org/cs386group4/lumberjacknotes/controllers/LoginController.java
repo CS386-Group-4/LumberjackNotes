460dfc0a0bc0036151767ffcabaeb27fda938a26
Nick Botticelli <nick.s.botticelli@gmail.com>
2021-04-09 17:31:01 -0700

    Added sign-up UI to LoginActivity

--- android_app/app/src/main/java/org/cs386group4/lumberjacknotes/controllers/LoginController.java
+++ android_app/app/src/main/java/org/cs386group4/lumberjacknotes/controllers/LoginController.java
@@ -1,10 +1,14 @@
 package org.cs386group4.lumberjacknotes.controllers;
 
+import android.animation.LayoutTransition;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.cs386group4.lumberjacknotes.R;
 import org.cs386group4.lumberjacknotes.ui.LoginActivity;
@@ -15,15 +19,50 @@ import org.cs386group4.lumberjacknotes.ui.NotesListActivity;
  */
 public class LoginController
 {
+    private boolean transitionedToStart = true;
+
     /**
      * Initialize the login controller
      * @param loginActivity valid instance to get a {@link Context} from
      */
     public LoginController(LoginActivity loginActivity)
     {
+        MotionLayout motionLayout = loginActivity.findViewById(R.id.login_container);
+
+        motionLayout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
         // Initialize login and register buttons
+        initMotionLayout(loginActivity, motionLayout);
         initLoginButton(loginActivity);
-        initRegisterButton(loginActivity);
+        initRegisterButton(loginActivity, motionLayout);
+    }
+
+    private void initMotionLayout(Activity activity, MotionLayout motionLayout)
+    {
+        TextInputLayout passwordConfirmContainer = activity.findViewById(R.id.passwordconfirm_field_container);
+
+        motionLayout.addTransitionListener(new MotionLayout.TransitionListener()
+        {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {}
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress)
+            {
+                // Enable or disable password confirmation text box
+                if (progress == 0)
+                    passwordConfirmContainer.setEnabled(false);
+                else if (progress == 1)
+                    passwordConfirmContainer.setEnabled(true);
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {}
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress)
+            {}
+        });
     }
 
     /**
@@ -47,15 +86,21 @@ public class LoginController
      * Initialize the register button such as by adding a click listener to it
      * @param loginActivity valid instance to get a {@link Context} from
      */
-    private void initRegisterButton(LoginActivity loginActivity)
+    private void initRegisterButton(LoginActivity loginActivity, MotionLayout motionLayout)
     {
         MaterialButton registerButton = loginActivity.findViewById(R.id.register_button);
 
         // Handle registration upon user clicking the register button
         registerButton.setOnClickListener(view ->
         {
-            // TODO: Handle registration with cloud
-            Toast.makeText(loginActivity, "Register button pressed", Toast.LENGTH_SHORT).show();
+            // Handle MotionLayout transition
+            if (transitionedToStart)
+                motionLayout.transitionToEnd();
+            else
+                motionLayout.transitionToStart();
+
+            // Set transition state
+            transitionedToStart = !transitionedToStart;
         });
     }
 }
